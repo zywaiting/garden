39c941603cc15d7bd9ed960e3450787dfe44cf26
wq_zy <1573240324@qq.com>
2018-09-13 21:02:27 +0800

    ...

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,39 +16,31 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+
+    //不管授不授权我都要自动登录获取openid
+    wx.login({
+      success: code => {
+        wx.request({
+          url: 'https://www.zhuyao.xin/onLoginAll',
+          data: {
+            code: code.code,
+            express: 'garden',
+            name: 'LoginUrl'
+          },
+          success: function (res) {
+            console.log(res.data.data);
+            console.log("--------success--------");
+            app.globalData.openid = res.data.data
+          },
+          fail: function (res) {
+            console.log("--------fail--------");
+          }
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+
+
+
+  
   }
 })
